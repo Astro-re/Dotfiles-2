b7f52aed4ac1f51400243f32dd360d1c630404a5
mocha <71246063+Astro-re@users.noreply.github.com>
2023-06-16 17:11:25 +0800

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+fish_add_path path /home/andre/.local/bin
+
+# ~/.config/fish/config.fish
+
+set -g fish_greeting
+
+
+starship init fish | source
+
+
+
+fish_add_path /home/andre/.spicetify
